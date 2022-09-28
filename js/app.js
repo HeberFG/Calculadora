f9d21bbf5f4138bcb44e01f23ea6fb19c7f77e6b
HeberFG <99307841+HeberFG@users.noreply.github.com>
2022-09-27 21:17:43 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,9 @@
-//investigar como registrar el sw, si estoy en local o prod
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register("/sw.js"); 
-}
-if (window.caches){    
-    caches.open('cache-v1')
-    .then((cache)=>{
-
-        // cache.add('/index.html')
-        // cache.add('/index.html')
-        // cache.add('/js/app.js')
-
-        cache.addAll([
-            '/index.html',
-            '/js/app.js'
-        ]).then(()=>{
-            cache.delete('/js/app.js')
-        })      
-    })
-
-    caches.keys().then(()=>{})
-
-    caches.has('cache-v2').then((r)=>{})
-
-    caches.delete('cache-v3');
+    
+    let thisUri = window.location;
+    if(thisUri.toString().includes('127.0.0.1')){
+        navigator.serviceWorker.register('/sw.js');
+    }else{
+        navigator.serviceWorker.register('/Calculadora/sw.js');
+    }
 }
