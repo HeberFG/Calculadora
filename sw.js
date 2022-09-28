0732107aa471ac299c6893e866572c07d081f8cd
HeberFG <99307841+HeberFG@users.noreply.github.com>
2022-09-27 21:44:50 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,25 @@
 self.addEventListener('install', (event) =>{
     console.log('SW: Intall');
-    caches.open('appShell').then((cache)=>{
-        cache.addAll([
-            '/Calculadora/',
-            '/Calculadora/index.html',
-            '/Calculadora/css/styles.css',
-            '/Calculadora/images/user-60.png',
-            '/Calculadora/images/dog2.png',
-            '/Calculadora/js/app.js',
-            '/Calculadora/js/calculadora.js',
-        ])
-    })
+    const promiseCache = caches.open('appShell').then((cache)=>{
+        return cache.addAll(
+            [
+                '/Calculadora/',
+                '/Calculadora/index.html',
+                '/Calculadora/css/style.css',
+                '/Calculadora/images/user-60.png',
+                '/Calculadora/images/dog2.png',
+                '/Calculadora/js/app.js',
+                '/Calculadora/js/calculadora.js',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+                 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
+            ]
+        );
+    });
+    
+    event.waitUntil(promiseCache);
 });
 
-self.addEventListener('fetch',(event)=>{
-    console.log("fetch") 
-})
+self.addEventListener('fetch', event =>{
+    const respCache = caches.match(event.request)
+    event.respondWith(respCache);
+});
