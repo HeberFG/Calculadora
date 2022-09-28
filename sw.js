93578235b2fb54bbf826304054f6b9f0079aa404
HeberFG <99307841+HeberFG@users.noreply.github.com>
2022-09-27 21:23:03 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,17 @@
-self.addEventListener('install',(event)=>{
-    console.log("instalado")
-})
-
-self.addEventListener('activate',(event)=>{
-    console.log("Activado")
-})
+self.addEventListener('install', (event) =>{
+    console.log('SW: Intall');
+    caches.open('appShell').then((cache)=>{
+        cache.addAll([
+            '/',
+            '/index.html',
+            '/css/styles.css',
+            '/images/user-60.png',
+            '/images/dog2.png',
+            '/js/app.js',
+            '/js/calculadora.js',
+        ])
+    })
+});
 
 self.addEventListener('fetch',(event)=>{
     console.log("fetch") 
