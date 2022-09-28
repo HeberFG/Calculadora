102135931ff79dee391f0b693debc930289a5604
HeberFG <99307841+HeberFG@users.noreply.github.com>
2022-09-27 22:32:21 -0500

    Update calculadora.js

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,25 +1,43 @@
 const sumar = () =>{
     console.log('Entró a sumar');
-    let num1 = document.getElementById('num1').value;
-    let num2 = document.getElementById('num2').value;
-    console.log(num1 + num2);
-    document.getElementById('resultado').value = (num1 + num2);
+    if(document.getElementById('num1').value == '' || document.getElementById('num2').value == ''){
+        alert('No se aceptan valores vacios')
+    }else{
+        let num1 = parseInt(document.getElementById('num1').value);
+        let num2 = parseInt(document.getElementById('num2').value);
+        document.getElementById('resultado').value = (num1 + num2);
+    }
 }
 
 const restar = () =>{
-    let num1 = document.getElementById('num1').value;
-    let num2 = document.getElementById('num2').value;
-    document.getElementById('resultado').value = (num1 - num2);
+    console.log('Entró a restar');
+    if(document.getElementById('num1').value == '' || document.getElementById('num2').value == ''){
+        alert('No se aceptan valores vacios')
+    }else{
+        let num1 = parseInt(document.getElementById('num1').value);
+        let num2 = parseInt(document.getElementById('num2').value);
+        document.getElementById('resultado').value = (num1 - num2);
+    }
 }
 
 const dividir = () =>{
-    let num1 = document.getElementById('num1').value;
-    let num2 = document.getElementById('num2').value;
-    document.getElementById('resultado').value = (num1 / num2);
+    console.log('Entró a dividir');
+    if(document.getElementById('num1').value == '' || document.getElementById('num2').value == ''){
+        alert('No se aceptan valores vacios')
+    }else{
+        let num1 = parseInt(document.getElementById('num1').value);
+        let num2 = parseInt(document.getElementById('num2').value);
+        document.getElementById('resultado').value = (num1 / num2);
+    }
 }
 
 const multiplicar = () =>{
-    let num1 = document.getElementById('num1').value;
-    let num2 = document.getElementById('num2').value;
-    document.getElementById('resultado').value = (num1 * num2);
+    console.log('Entró a multiplicar');
+    if(document.getElementById('num1').value == '' || document.getElementById('num2').value == ''){
+        alert('No se aceptan valores vacios')
+    }else{
+        let num1 = parseInt(document.getElementById('num1').value);
+        let num2 = parseInt(document.getElementById('num2').value);
+        document.getElementById('resultado').value = (num1 * num2);
+    }
 }
